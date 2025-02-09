ee112c83482c149098855a2c752b72f4cb80533c
gycxkani <454683496@qq.com>
2025-02-09 22:59:51 +0800

    add login page

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  
-  <div>
+  <!-- <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
+  </div> -->
   <nav>
     <router-link to="./home">Home</router-link> |
-    <router-link to="./about">About</router-link> |
+    <router-link to="./login">Login</router-link> |
+    <router-link to="./about">About</router-link> 
   </nav>
   <router-view></router-view>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
---
+++ src/views/Login.vue
@@ -0,0 +1,103 @@
+<template>
+  <div style="height: 100%">
+    <el-row style="height: 100%; background: #1aa094">
+      <el-col :lg="16" :md="12" class="col_left">
+        <h1>Vue3商城管理系统</h1>
+        <span>技术支持</span>
+      </el-col>
+      <el-col :lg="8" :md="12" class="col_right">
+        <h1>登录</h1>
+        <el-form
+          class="login-form"
+          ref="ruleFormRefLogin"
+          :model="ruleFormLogin"
+          :rules="rulesLogin"
+        >
+          <el-form-item prop="username">
+            <el-input
+              placeholder="请输入用户名"
+              v-model="ruleFormLogin.username"
+            >
+              <template #prefix>
+                <el-icon>
+                  <UserFilled />
+                </el-icon>
+              </template>
+            </el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input placeholder="请输入密码" v-model="ruleFormLogin.password">
+              <template #prefix>
+                <el-icon class="el-input__icon">
+                  <el-icon>
+                    <Lock />
+                  </el-icon>
+                </el-icon>
+              </template>
+            </el-input>
+          </el-form-item>
+        </el-form>
+        <el-button type="primary" @click="loginHandle" :loading="loading"
+          >登录</el-button
+        >
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive } from "vue";
+const ruleFormRefLogin = ref(null);
+const ruleFormLogin = reactive({
+  username: "",
+  password: "",
+});
+const rulesLogin = {
+  username: [
+    { required: true, message: "请输入用户名", trigger: "blur" },
+    { min: 6, max: 15, message: "长度应为6到15位之间", trigger: "blur" },
+  ],
+  password: [
+    { required: true, message: "请输入密码", trigger: "blur" },
+    { min: 6, max: 15, message: "长度应为6到15位之间", trigger: "blur" },
+  ],
+};
+const loginHandle = () => {
+  ruleFormRefLogin.value.validate((isValid) => {
+    if (!isValid) {
+      return;
+    }
+    // ...下一步
+  });
+};
+</script>
+
+<style lang="less" scoped>
+.col_left {
+  color: #fff;
+  display: flex;
+  // 垂直居中
+  align-items: center;
+  //水平居中
+  justify-content: center;
+  //flex方向修改成垂直方向
+  flex-direction: column;
+}
+
+.col_right {
+  background: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  .login-form {
+    width: 240px;
+  }
+}
+
+.el-button {
+  width: 240px;
+  background: #1aa094;
+}
+</style>
